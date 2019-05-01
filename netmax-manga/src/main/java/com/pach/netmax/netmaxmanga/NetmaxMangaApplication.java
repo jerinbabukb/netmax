50a05ddb74a0b9a1b935f0e923cbc10a77e10122
jerinbabukb <jerinbabukb@gmail.com>
2019-05-01 21:08:47 +0200

    initial commit

---
+++ netmax-manga/src/main/java/com/pach/netmax/netmaxmanga/NetmaxMangaApplication.java
@@ -0,0 +1,13 @@
+package com.pach.netmax.netmaxmanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetmaxMangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetmaxMangaApplication.class, args);
+	}
+
+}
---
+++ netmax-manga/src/test/java/com/pach/netmax/netmaxmanga/NetmaxMangaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pach.netmax.netmaxmanga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetmaxMangaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
